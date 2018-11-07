dcb4b5fd023ddd41cbf2d29b52c94d27fb950ed7
y-nakane <y-nakane@sakura.ad.jp>
2018-11-07 19:18:16 +0900

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 中根 佑介 on 2018/11/07.
+//  Copyright © 2018 usayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
