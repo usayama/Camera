e4b234c942f59850c41f0ce966e1cbb90391b2de
y-nakane <y-nakane@sakura.ad.jp>
2018-11-07 22:19:31 +0900

    Add Action Code in launchCamera

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -11,7 +11,14 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        let camera = UIImagePickerController.SourceType.camera
         
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
     }
     
     override func viewDidLoad() {
